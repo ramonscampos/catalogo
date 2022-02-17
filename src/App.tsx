eee3187b492bc7224dd055bb05281b9570d8db1d
Ramon Campos <ramonscampos@gmail.com>
2022-02-17 14:32:11 -0300

    Finalização

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StatusBar } from 'react-native';
 import AppProvider from 'hooks';
 import { Routes } from 'routes';
+import SplashScreen from 'react-native-splash-screen';
+
 import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
 import { Header } from 'components/Header';
 
 function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <AppProvider>
       <View style={{ flex: 1 }}>
