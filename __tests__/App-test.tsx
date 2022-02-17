314aa560d0c978e58aa00182191292ff5f9b19ca
Ramon Campos <ramonscampos@gmail.com>
2022-02-17 07:34:40 -0300

    Android fixes

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,14 +1,15 @@
-/**
- * @format
- */
-
 import 'react-native';
-import React from 'react';
-import App from '../App';
-
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import React from 'react';
+import {render, screen} from 'react-test-renderer'
+import App from '../src/App';
 
-it('renders correctly', () => {
+it('calculate the cart total', () => {
+  const
+
+  expect(screen.getByText(/^My Name Is:/)).toHaveTextContent(
+    'My Name Is: Unknown',
+  )
+
   renderer.create(<App />);
 });
